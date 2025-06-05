cc2daa54523f3946d8aa08b97052c0fdd10d7844
Vitor <vitorricardosilvabarbosa@gmail.com>
2025-06-04 21:27:18 -0300

    Inicializando projeto

---
+++ src/main/java/com/travel/belatur/BelaturApplication.java
@@ -0,0 +1,13 @@
+package com.travel.belatur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelaturApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelaturApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/travel/belatur/BelaturApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel.belatur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelaturApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
